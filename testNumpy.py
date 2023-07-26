448aeccb8ec522185c0e62c75a474bcd8698dc89
Md Shamim Reza <opt.shamim@gmail.com>
2023-07-27 01:24:57 +0600

    numpy updated

--- testNumpy.py
+++ testNumpy.py
@@ -7,3 +7,40 @@ print(arr)
 print(numpy.__version__)
 
 print(type(arr))
+
+
+# 0-D array
+arr = numpy.array(42)
+print(arr)
+
+#1-D array
+arr = numpy.array([9,8,7,6,5,4,3,2,1,0])
+print(arr)
+
+#2_D array
+arr = numpy.array([[99, 98, 97], [96, 95, 94]])
+print(arr)
+
+#3-D array
+arr = numpy.array([[[99, 98, 97], [96, 95, 94]], [[93, 92, 91], [90, 89, 88]]])
+print(arr)
+
+#check number of dimensions
+print(arr.ndim)
+
+
+#Access Array Elements
+arr = numpy.array([1, 2, 3, 4])
+print(arr[0])
+
+#Access 2-D Arrays
+arr = numpy.array([[1,2,3,4,5], [6,7,8,9,10]])
+print('2nd element on 1st dim: ', arr[0, 1])
+
+
+#Slicing arrays
+arr = numpy.array([1, 2, 3, 4, 5, 6, 7])
+print(arr[1:5])
+
+#Slice elements from index 4 to the end of the array
+print(arr[4:])
